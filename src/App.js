3215fcb097d5ea7dcb4173c1b01099c692fc272a
Philip <vietwhammies@gmail.com>
2023-03-22 00:56:51 +0100

    first commit of the new era

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+/* eslint-disable import/no-anonymous-default-export */
 import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
+import Modal from './components/Modal';
 
 const items = [
   {
@@ -34,13 +38,36 @@ const options = [
   },
 ];
 
-export default () => {
-  //const [selected, setSelected] = useState(options[0]);
-  //const [showDropdown, setShowDropdown] = useState(true);
 
+
+
+export default () => {
+  const [selected, setSelected] = useState(options[0]);
+  //const [showDropdown, setShowDropdown] = useState(true);
+  
   return (
     <div>
-      <Translate />
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown 
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
+      <Route path="/modal">
+        <Modal/>
+      </Route>
     </div>  
   );
 };
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,33 +1,37 @@
 import React, { useState } from 'react';
 
-const Accordion = ({ items }) => {
+function Accordion({ items }) {
   // strange HOOK syntax 
   /**
    * activeState = piece of state (1st term)
    * setActiveIndex = Function to change this piece of state (2nd term)
    * null = initial value for this piece of state 
    */
-  const [activeIndex, setActiveIndex] = useState(null);
+  const [expandedIndex, setExpandedIndex] = useState(-1);
 
-  const onTitleClick = (index) => {
-    setActiveIndex(index);
+  const handleClick = (nextIndex) => {
+    if (expandedIndex === nextIndex) {
+      setExpandedIndex(-1);
+    } else {
+      setExpandedIndex(nextIndex);
+    }
   }
 
   const renderedItems = items.map((item, index) => {
-    const active = index === activeIndex ? 'active' : '';
-    
+    const isExpanded = (index === expandedIndex) ? 'active' : '';
+
     // ` - backticks used for string templating preferred
     return (
       <React.Fragment key={item.title}>
         <div 
-          className={`title ${active}`}
-          onClick={() => onTitleClick(index)}
+          className={`title ${isExpanded}`}
+          onClick={() => handleClick(index)}
           >
           <i className="dropdown icon"></i>
           {item.title} 
         </div>
-        <div className={`content ${active}`}>
-          <p>{item.content}</p>
+        <div className={`content ${isExpanded}`}>
+          {isExpanded && <div>{item.content}</div>}
         </div>
       </React.Fragment>
     );
